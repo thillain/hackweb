a8720c4c0f56d8c73cbeefe6389d0519bf5a399a
Jude Arasu <judearasu@gmail.com>
2012-12-24 06:35:57 +0530

    color code changed

--- realest/assests/js/vendor/plugin.js
+++ realest/assests/js/vendor/plugin.js
@@ -1,4 +1,3 @@
 $(document).ready(function(){
-    $.getScript("/assests/js/vendor/modernizr.js");
-    $.getScript("/assests/js/vendor/less-1.3.0.min.js");
+    $.getScript("/assests/js/vendor/jquery.cslider.js");
 });
