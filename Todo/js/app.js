a95ca778987e938dc6b02f662e72b5369177aa16
Jude Arasu <judearasu@gmail.com>
2013-01-21 22:51:50 +0530

    sortable file pushed

--- Todo/js/app.js
+++ Todo/js/app.js
@@ -2,4 +2,4 @@ var source   = $("#entry-template").html();
 var template = Handlebars.compile(source);
 var context = {title: "My New Post", body: "This is my first post!"}
 var html    = template(context);
-$("#entry-template").html(html);
+$("#html-goes-here").html(template(context));
