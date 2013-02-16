c03460587845fbe3c3b8a2427942687501381ad4
JudeArasu <judearasu@genlinux.org>
2013-02-16 21:49:47 +0530

    nav link changes as per windows
    changed the nav path and used ajax cache clear

--- Sapient/assests/js/plugin.js
+++ Sapient/assests/js/plugin.js
@@ -7,3 +7,25 @@ $.getScript("assests/js/jquery.tablesorter.min.js", function() {
 	});
 });
 
+// Cache clear via Ajax
+$.ajaxSetup ({  
+        cache: false  
+    }); 
+
+// Default ajax call for page1
+
+$(function(){
+$.get('partials/page1.html',function(data) {
+$("section.main").html(data);
+})
+});
+
+// Ajax calls from nav links	
+var ajax_load = "<img src='img/load.gif' alt='loading...' />"; 
+$("a").click(function() {
+var url = $(this).attr('href');
+$.get(url,function(data) {
+$("section.main").html(data);
+})
+	return false;
+});
