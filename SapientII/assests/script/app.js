1fbad20c6b34165c938743ba8a953b87dc1b104c
JudeArasu <judearasu@gmail.com>
2013-06-30 18:56:50 +0530

    ember scripts

--- SapientII/assests/script/app.js
+++ SapientII/assests/script/app.js
@@ -1,7 +1,12 @@
 //<![CDATA[
 var src = document.getElementById("semantic-template").innerHTML;
 var tmpl = Handlebars.compile(src);
- $.getJSON("assests/json/carousel-data.json", function(data) {
-        $("#content-template").html(tmpl({objects:data}));
+$.getJSON("assests/json/carousel-data.json", function(data) {
+	$("#content-template").html(tmpl({objects:data}));
 });
 //]]>
+App=Ember.Application.create();
+App.Router.map(function(){
+	this.resource('project');
+	this.resource('about');
+});
