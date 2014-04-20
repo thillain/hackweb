a4e4d5bbb80a4ac4cb3117b3f05fef8cbba18f79
JudeArasu <judearasu@gmail.com>
2014-04-20 15:06:19 +0530

    done

--- hackearth/app.js
+++ hackearth/app.js
@@ -3,5 +3,39 @@
 	$( "a.btn" ).on( "click", function() {
 		$('#myModal').modal();
 	});
+	$('#myTab a:last').tab('show')
+	$("form").on("submit",function(event){
+       // Stop form from submitting normally
+       event.preventDefault();
+       var $form = $(this),
+       name=$form.find("#inputname").val(),
+       track=$form.find("#inputtracknumber").val();
+       $form.hide();
+
+
+       $.ajax({
+       	type: "GET",
+       	url: "http://itunes.apple.com/search",
+       	data: { term: name, limit: track },
+       	dataType: 'jsonp'
+       })
+       .done(function(data) {
+       	$('#myModal').modal();
+       	var items = [], 
+       	    contents =[];
+       	$.each(data.results, function (i,artist) {
+       	    items.push( "<li><a href='#" + artist.artistName+ "'>" + artist.artistName + "</a></li>" );
+       	});
+       	$("#myTab").html(items);
+       	$.each(data.results,function(i,artist){
+            contents.push("<div class='tab-pane'>"+artist.artistName+"</div>");
+       	});
+       	$("#tab-content").html(contents);
+       	
+       });
+
+
+       
+   });
 	
 })(jQuery);
